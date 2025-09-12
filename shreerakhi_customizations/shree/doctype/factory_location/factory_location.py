9175d4934a9a72473d558a1f8c37af92e8f9ece4
Atulsah <atul.sah7@gmail.com>
2025-09-12 07:01:33 +0000

    Initial commit for ERPNext custom app

---
+++ shreerakhi_customizations/shree/doctype/factory_location/factory_location.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, atul and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FactoryLocation(Document):
+	def autoname(self):
+		self.name = f"{self.custom_name}"
