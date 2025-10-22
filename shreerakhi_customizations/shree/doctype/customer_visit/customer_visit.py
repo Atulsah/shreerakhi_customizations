7a0de0fdd1221c37a7eae8404d96a72490893596
Atulsah <atul.sah7@gmail.com>
2025-10-22 09:00:18 +0000

    Customer Visit Updated

--- shreerakhi_customizations/shree/doctype/customer_visit/customer_visit.py
+++ shreerakhi_customizations/shree/doctype/customer_visit/customer_visit.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, atul and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.model.naming import make_autoname
 
 class CustomerVisit(Document):
-	pass
+    def autoname(self):
+        # Ensure year is selected
+        if not self.year:
+            frappe.throw("Please select Year before saving.")
+
+        # Build prefix dynamically
+        prefix = f"CV-{self.year}-"
+
+        # Use frappe's naming_series function to auto increment
+        self.name = make_autoname(prefix + ".#####")
